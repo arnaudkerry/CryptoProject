e006e0f909c46a490fff292f7bdc1260e60af2bf
arnaudkerry <arnaudkerry@gmail.com>
2023-01-09 10:03:58 +0100

    Multithreadi

--- check_duplicate_key.py
+++ check_duplicate_key.py
@@ -1,18 +1,33 @@
+import subprocess
 import sys
-from cryptography.hazmat.primitives import serialization
-
+import itertools
+import threading
+import time
 
 def check_duplicate(file):
-    with open(file, "rb") as f:
-        private_key = serialization.load_pem_private_key(
-            f.read(),
-            password=None
-        )
-        print(private_key.private_bytes())
-    return
+    f = open(file, "rb")
+    infos = f.readline().decode("utf-8").split()
+    nbKey = int(infos[0])
+    keys = f.read().split(b"-----END RSA PRIVATE KEY-----")
+    for a,b in itertools.combinations(keys, 2):
+        threading.Thread(target=compare,args=(a,b)).start()
+
+def compare(a,b):
+    if a == b:
+        print("Found ! : %s" % a)
+        return -1
+    
+
+def format_Text(file):
+    subprocess.call(["sed '/^-----BEGIN RSA PRIVATE KEY-----/d' %s > out.txt" % file], shell=True)
+    start = time.time()    
+    check_duplicate("out.txt")
+    print("--- %s seconds ---" % (time.time() - start))
+    subprocess.call(["rm out.txt"], shell=True)
+
 
 if __name__ == "__main__":
     if(len(sys.argv) == 2):
-        check_duplicate(sys.argv[1])
+        format_Text(sys.argv[1])
     else:
         print("Usage : python3 check_duplicate_key.py FILE")
--- key_generator.py
+++ key_generator.py
@@ -1,26 +1,34 @@
+import threading
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
 import sys
 import os
+import time
 
-def gen_key(bits: int = 512):
+def gen_key(bufferFile,bits: int = 512):
+    
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=bits
     )
-    return private_key.private_bytes(encoding=Encoding.PEM,format=PrivateFormat.TraditionalOpenSSL,encryption_algorithm=NoEncryption())
+    bufferFile.write(private_key.private_bytes(encoding=Encoding.PEM,format=PrivateFormat.TraditionalOpenSSL,encryption_algorithm=NoEncryption()))
 
-def generate(nb, bits = 512):
+def generate(nb, bits = "512"):
     if os.path.exists("keyfile.txt"):
         os.remove("keyfile.txt")
     f = open("keyfile.txt", "ab+")
+    line = nb+" \n"
+    f.write(bytes(line, "utf-8"))
     for i in range(int(nb)):
-        f.write(gen_key(bits))
+        threading.Thread(target=gen_key,args=(f,int(bits))).start()
+        
 
 if __name__ == "__main__":
     if(len(sys.argv) == 1):
         print("Usage : key_generator n [bits]")
     if(len(sys.argv) == 2):
-        generate(int(sys.argv[1]))
+        start = time.time()
+        generate(sys.argv[1])
+        print("--- %s seconds ---" % (time.time() - start))
     if(len(sys.argv) == 3):
-        generate(int(sys.argv[1]), int(sys.argv[2]))
+        generate(sys.argv[1], int(sys.argv[2]))
